0faeadb25a9671d6d4ae7fb99c194657f6471b61
Naor W <106400586+Easymoney13@users.noreply.github.com>
2023-06-06 18:03:20 +0300

    Create helper.h

---
+++ assrmbler files/helper.h
@@ -0,0 +1,8 @@
+#ifndef HELPER_H
+#define HELPER_H
+#include "store.h"
+void is_successful_malloc(void*);
+char* return_ob_file_name(char* name);
+char* return_ent_file_name(char* name);
+char* return_ext_file_name(char* name);
+#endif
